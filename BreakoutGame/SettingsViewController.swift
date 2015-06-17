45d950a2d3b49d29e879a8763cac1cef6c220141
Brord van Wierst <brordvanwierst@msn.com>
2015-06-17 06:11:03 -0700

    Added settingsViewController

---
+++ BreakoutGame/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  BreakoutGame
+//
+//  Created by Mad Max on 17/06/15.
+//  Copyright (c) 2015 nl.han.ica.mad. All rights reserved.
+//
+
+import Foundation
+
+class SettingsViewController: UIViewController {
+    
+}
