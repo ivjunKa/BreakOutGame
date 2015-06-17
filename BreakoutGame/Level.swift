dbabff2f30af81f26b0cf3c5e9901f9c743d298e
ivjunka <pronl@hotmail.com>
2015-06-17 03:32:50 -0700

    begin for the level info

--- BreakoutGame/Level.swift
+++ BreakoutGame/Level.swift
@@ -7,16 +7,41 @@
 //
 
 import Foundation
+import SpriteKit
 
 class Level {
     
     var levelName: String?
+    var nrOfRows:Int?
+    var nrOfCols:Int?
+    var padding:CGFloat?
+    var colWidth:CGFloat?
     
+    class brickPositions {
+        var brickCoords: CGPoint?
+        var brickTypeName: String?
+        var brickImg: String?
+        init(brickCoords: Int, brickTypeName: String, brickImg: String){
+            
+        }
+    }
     init(levelName name: String){
         self.levelName = name
+        prepareLevelStructure(self.levelName!)
     }
     
     func getLevelName() -> String {
         return levelName!
+    }
+    func prepareLevelStructure(levelName: String){
+        switch levelName {
+        case "1-1":
+            self.nrOfRows = 3
+            self.nrOfCols = 3
+            self.padding = 20
+            
+            
+        default: println("1-1")
+        }
     }
 }
