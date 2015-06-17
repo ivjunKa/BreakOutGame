b095bb15d72da06587d2fcff6d1da83483ecb636
Brord van Wierst <brordvanwierst@msn.com>
2015-06-17 06:43:59 -0700

    Added settings bundle - Doesnt work ignore it. Edit SettingsViewCOntroller instead

--- BreakoutGame/SettingsViewController.swift
+++ BreakoutGame/SettingsViewController.swift
@@ -6,8 +6,18 @@
 //  Copyright (c) 2015 nl.han.ica.mad. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    
+    @IBAction func numLives(sender: UISlider) {
+        
+    }
+    
+    @IBAction func startingBalls(sender: UISegmentedControl) {
+        
+    }
+    
     
 }
